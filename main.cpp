1934a3425a6b8d1b2e934747f80513ebf7a11261
WEI-CHE <wery6129@gmail.com>
2026-07-16 14:47:24 +0800

    update add2 function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int add2(int a, int b) {
+    return a + b;
+}
+
 int main(){
-    int n;
-    cin >> n;
-    cout << "Hello World" << endl;
+    int n, m;
+    cin >> n >> m;
+    cout << add2(n, m) << endl;
 }
